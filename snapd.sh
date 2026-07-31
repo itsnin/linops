079862a199d6b772d15a0a9e3fe5be59e0d04eb4
Nahian I. Nafseen <ninx.sh@gmail.com>
2026-07-31 16:22:54 +0600

    Delete snapd.sh

--- snapd.sh
+++
@@ -1,3 +0,0 @@
-sudo apt-get remove --purge -y snapd      # Kill snapd + configs
-sudo apt-get autoremove -y                # Clean orphaned deps
-sudo apt-mark hold snapd                  # PREVENT REINSTALL
